e3e8fa02d8c2b173a2203623c18de2c949efb05b
Nitish Jaiswal <141999440+nitishjaiswal2002@users.noreply.github.com>
2024-05-31 17:52:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let labels = document.querySelectorAll('.input'); // Get all elements with class 'input'
+labels.forEach((label, index) => {
+  if (index % 2 === 1) { // Check if the element is even (index starts from 0)
+    label.style.color = 'red'; // Change the color of the text in the 'input' element
+  }
+});
